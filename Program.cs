d61fca3a3a91e7009964f7086b6295fbfea754fe
Andrey <web__master@list.ru>
2022-05-06 22:17:32 +0500

    Add maket blok-schema

--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ string[] Array = new string[] {"hello", "2", "world", ":-)"};
 // создаю новый массив в который будут складываться нужные значения. Длинна нового массив равна длинне заданного массива Array
 string[] NewArray = new string [Array.Length];
 
+void all (string [] array, string [] newarray)
+{
+
+}
+
 //Метод печати массива
 void PrintArray ()
     {
