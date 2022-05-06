065cf58af0e9810528b29a49e9e5287573ffc416
Andrey <web__master@list.ru>
2022-05-06 21:43:13 +0500

    Add void PritnArray and Array, NewArray

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//string Array = Console.ReadLine(); //получение строки с клавиатуры
+
+// задаю заданный условием массив с имеющимеся значениями
+string[] Array = new string[] {"hello", "2", "world", ":-)"};
+
+// создаю новый массив в который будут складываться нужные значения. Длинна нового массив равна длинне заданного массива Array
+string[] NewArray = new string [Array.Length];
+
+//Метод печати массива
+void PrintArray ()
+    {
+        Console.Write("[ ");
+        for (int i = 0; i < Array.Length; i++)
+        {
+            Console.Write($"{Array[i]} ");
+        }
+        Console.Write("]");
+        Console.WriteLine();
+    }
+    PrintArray();
